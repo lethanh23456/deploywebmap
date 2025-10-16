a8fb4c18ffd33235181ec52a241c074f21c602c9
lethanh23456 <thanhld2006.ptit@gmail.com>
2025-10-17 01:39:35 +0700

    up

--- src/shared/components/avatar/AvatarMenu.tsx
+++ src/shared/components/avatar/AvatarMenu.tsx
@@ -2,6 +2,7 @@ import { Box, Menu, Text } from "@mantine/core";
 import Avatar from "./Avatar";
 import { Link } from "@tanstack/react-router";
 import { avatarMenuConfig } from "@/app/configs/global/avatar-menu.config";
+import avatarImage from "@/assets/anh1.png"; 
 
 const AvatarMenu = () => {
   const user = {
@@ -12,7 +13,9 @@ const AvatarMenu = () => {
     <Menu position="bottom-start" width={200}>
       <Menu.Target>
         <Box className="flex items-center gap-2 cursor-pointer">
-          <Avatar />  
+          <Avatar 
+            image={avatarImage}
+          />
           {/* <Box visibleFrom="xl">
             <Text>{user.name ?? "Yukihara"}</Text>
           </Box> */}
